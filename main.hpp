016f326077b198274801f505e233a4ba3f67a92c
paolocs110 <pciappetta@students.avc.edu>
2024-03-17 23:28:54 -0700

    complete quesiton 1

--- main.hpp
+++ main.hpp
@@ -9,19 +9,21 @@ using namespace std;
 // Implement all your functions here
 // ******************************
 
+void getTwoValues(int &, int &);
+
 void getTwoValues(int &n1, int &n2) {
     do {
         cout << "Enter the values for begin, end: ";
         cin >> n1 >> n2;
-    } while (n1 >= n2);
+    } while ((n1 >= n2));
 }
 
 int getNextPrime(int n) {
+    n++;
     bool isPrime = false;
-    while (isPrime = false) {
-        cout << n;
-        for (int i = 2; i < n; i++) { //Chkeck if n is prime
-            if ((n % i) == 0) {
+    while (isPrime == false) {
+        for (int i = 2; i <= n; i++) { //Chkeck if n is prime
+            if (((n % i) == 0) && n != 2) {
                 break;
             }
             if (i > (n/2)) {
@@ -35,5 +37,17 @@ int getNextPrime(int n) {
 }
 
 int getPrevPrime(int n) {
-    return 1;
+    n--;
+    bool isPrime = false;
+    while (isPrime == false) {
+        for (int i = 2; i <= n; i++) { //Chkeck if n is prime
+            if (((n % i) == 0) && n != 2) {
+                break;
+            }
+            if (i > (n/2)) {
+                return n;
+            }
+        }
+        n--;
+    }
 }
