4c0374d07e159aa615ec4e623c657b701e454fd9
paolocs110 <pciappetta@students.avc.edu>
2024-03-17 22:23:18 -0700

    first run test, added getTwoValues

--- main.hpp
+++ main.hpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 
 //********************
 // Declare Function Prototypes
@@ -6,3 +8,18 @@
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void getTwoValues(int &n1, int &n2) {
+    do {
+        cout << "Enter the values for begin, end: ";
+        cin >> n1 >> n2;
+    } while (n1 >= n2);
+}
+
+int getNextPrime(int n) {
+    return 1;
+}
+
+int getPrevPrime(int n) {
+    return 1;
+}
